3851733ec12bd1d9165441f0d59ff45a94a589b6
Bartosz <bprzygoda@gmail.com>
2024-10-28 10:10:49 +0100

    cmd on the left hand, the big one is not working :/

--- src/keyboard/ergodox/layout/colemak-kinesis-mod.c
+++ src/keyboard/ergodox/layout/colemak-kinesis-mod.c
@@ -39,9 +39,9 @@ _backslash,   _Q,         _W,      _F,      _P,    _G,     _9,
             _H,      _N,      _E,      _I,         _O,    _quote,
 _semicolon, _K,      _M,  _comma, _period,     _slash,   _shiftR,
                _arrowL, _arrowD, _arrowU,    _arrowR,    _F10,
-     _ctrlR, _altL,
+     _ctrlR, _guiR,
 1,      0,      0,
-_altR, _guiR, _enter ),
+_altR, _guiL, _enter ),
 
 
 	KB_MATRIX_LAYER(  // layout: layer 1: function and symbol keys
